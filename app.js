47a52a1cfb9ffa83b62863487ce65661314ca9f7
berkbakkaloglu <148460203+berkbakkaloglu@users.noreply.github.com>
2024-06-05 17:20:21 +0300

    moongose fix

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ admin.initializeApp({
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
 const bucket = admin.storage().bucket();
 
 app.use(bodyParser.urlencoded({ extended: true }));
